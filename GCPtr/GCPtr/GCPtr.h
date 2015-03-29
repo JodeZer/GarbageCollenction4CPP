97e42c3daf6ab5d309512e4f6f58574521f19a39
JodeZer <jeffery1993@hotmail.com>
2015-03-29 23:39:47 +0800

    2nd submission

--- GCPtr/GCPtr/GCPtr.h
+++ GCPtr/GCPtr/GCPtr.h
@@ -7,8 +7,56 @@ class GCPtr
 {
 public:
 	static list<GCPInfo<T>> sm_GCPList;//c++11新标准
+	GCPInfo<T> m_this;
+	static bool isFirst;//不知道什么意思，照抄的
 public:
-	GCPtr(void);
-	virtual ~GCPtr(void);
+	GCPtr(T *p=nullptr,int size=0)
+	{
+		if(first)
+			atexit(shutdown);//不知道什么意思，照抄的
+		first=false;
+		m_this=new GCPInfo(p,size);
+		list<GCPInfo<T>>::iterator it;
+		it=find(p);
+		if(it==sm_GCPList.end())
+			sm_GCPList.push_front(m_this);
+		else
+			it->m_refCount++;
+
+
+	}
+	virtual ~GCPtr(void)
+	{
+		list<GCPInfo<T>>::iterator it=find(m_this.m_menPtr);
+		it->m_refCount--;
+	}
+
+private:
+	typename list<GCPInfo<T>>::iterator find(T *ptr)//typename的目的是告诉编译器这是一个类型，否则不能通过语法检查
+	{
+		list<GCPInfo<T>>::iterator it;
+
+		for(it=sm_GCPList.begin();it!=sm_GCPList.end();it++)
+		{
+			if(it->m_menPtr==ptr)
+				return it;
+		}
+		return it;
+
+	}
+
+	void collect(void)
+	{
+		list<GCPInfo<T>>::iterator it;
+		for(it=sm_GCPList.begin();it!=sm_GCPList.end();it++)
+		{
+			if(it->m_refCount<=0)
+			{
+				delete it->m_menPtr;
+				sm_GCPList.erase(it);
+			}
+		}
+	}
+
 };
 
--- GCPtr/GCPtr/main.cpp
+++ GCPtr/GCPtr/main.cpp
@@ -6,14 +6,17 @@ using namespace std;
 
 void main(void)
 {
-	int *p=new int(2);
+	list<int> l;
+	list<int>::iterator it;
+	it=l.begin();
+	/*int *p=new int(2);
 	int *p2=p;
 	GCPInfo<int> a(p,0);
 	GCPInfo<int> b(p2);
 	cout<<(int)(a==b);
 	p2=new int(2);
 	GCPInfo<int> c(p2);
-	cout<<(int)(a==c);
+	cout<<(int)(a==c);*/
 	
 	return;
 }
